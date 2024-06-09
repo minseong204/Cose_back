d895c18edf3a142b8e990a8b82e0cfb54de49dbb
minseong204 <minseong2083@icloud.com>
2024-06-09 16:24:48 +0900

    fix : 이메일 존재 여부 응답 데이터 변경

--- src/main/java/com/min204/coseproject/exception/ExceptionControllerAdvice.java
+++ src/main/java/com/min204/coseproject/exception/ExceptionControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.min204.coseproject.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,18 +12,18 @@ import java.util.Map;
 public class ExceptionControllerAdvice {
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
-    public ResponseEntity<Map<String, Object>> handleEmailAlreadyExistsException(EmailAlreadyExistsException exception) {
-        Map<String, Object> errorDetails = new HashMap<>();
-        errorDetails.put("success", false);
-        errorDetails.put("message", exception.getMessage());
-        errorDetails.put("data", Map.of(
-                "email", exception.getEmail(),
+    public ResponseEntity<Map<String, Object>> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("success", false);
+        response.put("message", "이미 사용중인 이메일입니다.");
+        response.put("data", Map.of(
+                "email", ex.getEmail(),
                 "isAvailable", false,
-                "Type", exception.getType()
+                "Type", ex.getType()
         ));
-
-        return ResponseEntity.badRequest().body(errorDetails);
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
     }
+
 
     @ExceptionHandler(BusinessLogicException.class)
     public ResponseEntity<Map<String, Object>> handleBusinessLogicException(BusinessLogicException exception) {
