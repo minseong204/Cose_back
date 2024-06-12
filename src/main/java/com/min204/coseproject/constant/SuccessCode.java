6150bd381712dcd10c814c9e7a96eec95851b3a9
minseong204 <minseong2083@icloud.com>
2024-06-13 01:27:57 +0900

    feat : 팔로우 관련 응답 객체 추가

--- src/main/java/com/min204/coseproject/constant/SuccessCode.java
+++ src/main/java/com/min204/coseproject/constant/SuccessCode.java
@@ -5,10 +5,13 @@ import lombok.Getter;
 @Getter
 public enum SuccessCode implements BodyCode {
     SUCCESS( "성공!", "Done"),
-    SIGN_UP_SUCCESS("201CREATED", "회원가입성공"),
+    SIGN_UP_SUCCESS("201 CREATED", "회원가입 성공"),
     LOGIN_SUCCESS("202 ACCEPTED", "로그인 성공"),
-    REISSUE_SUCCESS("200 SUCCESS", "재발행 성공"),
-    EMAIL_AVAILABLE("200 SUCCESS", "이메일 사용가능")
+    REISSUE_SUCCESS("200 OK", "토큰 재발행 성공"),
+    EMAIL_AVAILABLE("200 OK", "이메일 사용 가능"),
+    FOLLOW_SUCCESS("200 OK", "팔로우 성공"),
+    UNFOLLOW_SUCCESS("200 OK", "언팔로우 성공"),
+    FETCH_SUCCESS("206 PARTIAL_CONTENT", "정보 조회 성공")
     ;
     private final String code;
     private final String message;
